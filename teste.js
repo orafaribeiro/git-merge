87b270f264ac46a2cb03c1be3671c406e31b018e
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-03-27 17:21:13 -0300

    Revert "Revert "ok""
    This reverts commit 9676dfb1cd40eaf5702fabfee449c5ea1c212834.

--- teste.js
+++ teste.js
@@ -1,3 +1,5 @@
 console.log("OK");
 console.log("OK2");
 console.log("OK5");
+console.log("OK3");
+console.log("OK4");
---
+++ teste2.js
@@ -0,0 +1,4 @@
+console.log("OK")
+console.log("OK2")
+console.log("OK3")
+console.log("OK4")
