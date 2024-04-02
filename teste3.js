5b01279a58ef8e6ced2c6417f8742adce2f7640e
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-04-02 08:36:44 -0300

    ok

--- teste3.js
+++ teste3.js
@@ -4,6 +4,7 @@ console.log("TESTE");
 console.log("TESTE");
 console.log("TESTE");
 console.log("TESTE");
+console.log("TESTE");
 console.log("TESTE2");
 console.log("TESTE2");
 console.log("TESTE2");
